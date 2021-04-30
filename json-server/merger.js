ce23edf2f00633b8bcd04c6c84c71ccf2a3f7620
gmdr1024 <gmdr1024@gmail.com>
2021-04-30 22:45:57 +0900

    Update files to be enable dummy-json
    #14

--- json-server/merger.js
+++ json-server/merger.js
@@ -1,10 +1,12 @@
 const jsonMerger = require("json-merger")
+const dummyjson = require('dummy-json')
 const fs = require('fs')
 
-const jsonFiles = fs.readdirSync('db')
-jsonFiles.forEach((value, index) => {
-  jsonFiles[index] = 'db/' + value
+const hbsFiles = fs.readdirSync('db')
+hbsFiles.forEach((value, index) => {
+  const template = fs.readFileSync('db/' + value, { encoding: 'utf8' })
+  hbsFiles[index] = JSON.parse(dummyjson.parse(template))
 })
-const result = jsonMerger.mergeFiles(jsonFiles)
+const result = jsonMerger.mergeObjects(hbsFiles)
 
 fs.writeFileSync('db.json', JSON.stringify(result))
